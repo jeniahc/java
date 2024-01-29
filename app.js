44f5881635c48811421461a3b3c6a894eab5a893
jeniahc <123027522+jeniahc@users.noreply.github.com>
2024-01-28 19:18:51 -0600

    variables

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Do you have any pets?");
+// Global Variable
+var dogName = "Rufus";
+
+var dogName = "Rosie";
+
+// Block Variable
+let g = 2;
+
+{
+    let g = 5;
+}
+
+// String Variable
+let Statement = "Dogs are adorable";
+
+// Object Variable
+const date = new Date();
+
+// Mathematics
+let a = 2;
+let b = a ** 5;
+
+
+console.log(Statement);
